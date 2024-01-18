9883fd8f3b55f8c6a12daa270f9bbaac55b167e3
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-18 08:51:06 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a
+        a = Val(TextBox1.Text)
+        If a Mod 2 = 0 Then
+            TextBox2.Text = "even"
+        Else
+            TextBox1.Text = "odd"
+        End If
+    End Sub
+End Class
